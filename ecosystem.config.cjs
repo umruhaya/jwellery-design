2a0f1f3fbe700d85a8316cb68986d3d4396c3a58
Umer Naeem <umernaeem135acc@gmail.com>
2025-07-08 12:55:33 +0500

    pm2 config fix env not loading

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,15 +1,15 @@
 module.exports = {
-  apps: [
-    {
-      name: "cyodesign",
-      script: "dist/server/entry.mjs",
-      interpreter: "node",
-      args: "--env-file=.env",
-      cwd: "/home/umernaeem135acc/cyodesign",
-      env: {
-        NODE_ENV: "production",
-        PORT: '8000'
-      }
-    }
-  ]
-};
+	apps: [
+		{
+			name: 'cyodesign',
+			script: 'dist/server/entry.mjs',
+			interpreter: 'node',
+			node_args: '--env-file=.env',
+			cwd: '/home/umernaeem135acc/cyodesign',
+			env: {
+				NODE_ENV: 'production',
+				PORT: '8000',
+			},
+		},
+	],
+}
